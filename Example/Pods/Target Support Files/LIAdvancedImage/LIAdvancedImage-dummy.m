0200cdc24aef59ad4d76bd5e2cbbf364ae74009c
Lehlohonolo Mbele <lehlohonoloisaac25@gmail.com>
2018-06-19 19:52:07 +0200

    Initial commit with pod lib template

---
+++ Example/Pods/Target Support Files/LIAdvancedImage/LIAdvancedImage-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LIAdvancedImage : NSObject
+@end
+@implementation PodsDummy_LIAdvancedImage
+@end
---
+++ Example/Pods/Target Support Files/Pods-LIAdvancedImage_Example/Pods-LIAdvancedImage_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LIAdvancedImage_Example : NSObject
+@end
+@implementation PodsDummy_Pods_LIAdvancedImage_Example
+@end
